d8e1c484586e5e547e3450caf120e26e5fe338d0
Eugene Krabs <tyler.smith11407@gmail.com>
2022-06-24 14:41:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,392 +1,303 @@
-from bs4 import BeautifulSoup
 import requests
 import urllib.request
 from urllib.request import Request, urlopen
-import dicttoxml
+import random
 import json
 import gtts
 from pygame import mixer
-from board import SCL, SDA
-import busio
-from oled_text import OledText
 import time
-import re
-from mutagen.mp3 import MP3
-import RPi.GPIO as GPIO
+from python_translator import Translator
+#from mutagen.mp3 import MP3
+#import RPi.GPIO as GPIO
+import winsound
 
-
-i2c = busio.I2C(SCL, SDA)
-oled = OledText(i2c, 128, 64) # create oled
 mixer.init() # pygame audio
 
-one = 14
-zero = 25
-space = 8
-enter = 7
+one = 26
+zero = 19
+space = 6
+enter = 5
+vb = 15
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(one, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(zero, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(space, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(enter, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(one, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#GPIO.setup(zero, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#GPIO.setup(space, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#GPIO.setup(enter, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#GPIO.setup(vb, GPIO.OUT)
 
-i = 0
-oled.text('____ADDERALL____', 1)
-oled.text('- DICTIONARY', 2)
-oled.text('  SPANISH', 3)
-oled.text('  READ TXT', 4)
-oled.auto_show = False
+translator = Translator()
 
-while True:
-	up_high = GPIO.input(one)
-	if up_high == True:
-		i -= 1
-		print('up {}'.format(i))
-		time.sleep(.4)
+def input_gpio():
+		# create dictionary
+		character = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
+		code = ['01','1000','1010','100','0','0010','110','0000','00','0111','101','0100','11','10','111','0110','1101','010','000','1','001','0001','011','1001','1011','1100','11111','01111','00111','00011','00001','00000','10000','11000','11100','11110']
+		phrase_lst = []
+		phrase_str = ''
 
-	down_high = GPIO.input(zero)
-	if down_high == True:
-		i += 1
-		print('down {}'.format(i))
-		time.sleep(.4)
+		while True:
+			usr_input = input('Enter Code:' )
+			phrase_lst.append(usr_input)
+			break
 
-	enter_high = GPIO.input(enter)
-	if enter_high == True:
-		oled.text('', 2)
-		oled.text('', 3)
-		oled.text('', 4)
+			#one_high = GPIO.input(one)
+			#if one_high == True:
+				#print('1')
+				#phrase_lst.append('1')
+				#time.sleep(.4)
+
+			#zero_high = GPIO.input(zero)
+			#if zero_high == True:
+				#print('0')
+				#phrase_lst.append('0')
+				#time.sleep(.4)
+
+			#space_high = GPIO.input(space)
+			#if space_high == True:
+				#print('*')
+				#phrase_lst.append('*')
+				#time.sleep(.4)
+
+			#enter_high = GPIO.input(enter)
+			#if enter_high == True:
+				#print('Enter')
+				#break
+
+		phrase = ''.join(phrase_lst)
+		#print(phrase)
+
+		# reverse the previous dict as it's easier to access the keys
+		zipped_code_char = zip(code,character)
+		rev_morse_dict = dict(list(zipped_code_char))
+		# initiating a while loop
+		while True:
+			# empty list to store original message
+			ori_msg = []
+			# empty list to store decoded message
+			dec_msg = []
+			# append input_msg (string) to ori_msg (string)
+			ori_msg.append(phrase)
+			# split each morse code by '*'
+			new_msg = phrase.split("*")
+
+			# printing out the original message
+			for line in ori_msg: # to print original message without the []
+				print("Original message: " + line)
+
+			# loop through each morse code representation
+			for j in range (0,len(new_msg)):
+				# get the alphanumeric alphabet based on the dict keys and append to dec_msg
+				if new_msg[j] in rev_morse_dict.keys():
+					dec_msg.append(rev_morse_dict[new_msg[j]])
+
+			# printing out the decoded message
+			dec_msg_str = ''.join(dec_msg)
+			print ("Decoded Message is: " + ''.join(dec_msg))
+			break
+		return(dec_msg_str)
+		
+def math_solver():
+	frac_open = '{'
+	frac_close = '}'
+	dec_msg_str = 'x+6=9+{4/7}+{(19+1)/(17-5)}'
+	
+	
+	math_url = dec_msg_str[dec_msg_str.find(frac_open)+1 : dec_msg_str.find(frac_close)]
+
+
+	
+	print('https://mathsolver.microsoft.com/en/solve-problem/{}'.format(dec_msg_str))
+	page = requests.get('https://api.dictionaryapi.dev/api/v2/entries/en_US/{}'.format(dec_msg_str))
+	content = page.text
+	print(content)
+
+def dictionary(dec_msg_str):
+	try:
+		print('https://api.dictionaryapi.dev/api/v2/entries/en_US/{}'.format(dec_msg_str))
+		page = requests.get('https://api.dictionaryapi.dev/api/v2/entries/en_US/{}'.format(dec_msg_str))
+	except:
+		print('404')
+		#mixer.music.load('404.mp3') # 404 not found
+		#mixer.music.play()
+		#time.sleep(3)
+		#GPIO.cleanup()
+		exit()
+
+	content = page.text
+
+	try:
+		results = json.loads(content)
+		#print(results)
+	except:
+		print('400')
+		#mixer.music.load('400.mp3') # 400 bad request
+		#mixer.music.play()
+		#time.sleep(3)
+		return False
+
+	r_pos = []
+	r_def = []
+	for obj in results:
+		r_word = obj['word']
+		for key in obj['meanings']:
+			r_pos.append(key['partOfSpeech'])
+			for key in key['definitions']:
+				r_def.append(key['definition'])
+
+	#r_str = '{}, {}: {}'.format(r_word[0], r_pos[0], r_def[0])
+	r_str = r_def[0]
+	print(r_str)
+	vb_motor_lst((ph_to_morse(r_str)))
+	#tts = gtts.gTTS(r_str, lang='en')
+	#audio_play(tts_save(dec_msg_str, tts))
+
+def spanish(dec_msg_str):
+	result = translator.translate(dec_msg_str, 'english')
+	if str(result) == dec_msg_str:
+		result = translator.translate(dec_msg_str, 'spanish')
+
+	r_str = '{}: {}'.format(dec_msg_str, result)
+	print(r_str)
+	r_str = str(result)
+	vb_motor_lst((ph_to_morse(r_str)))
+	#tts = gtts.gTTS(r_str, lang='en')
+	#audio_play(tts_save(dec_msg_str, tts))
+
+def text_doc():
+	count = -1
+	with open('doc.txt', 'r', encoding='utf-8') as o:
+		lines = o.readlines()
+		while True:
+			#one_high = GPIO.input(one)
+			#if one_high == True:
+			inl = input('Line: ')
+			if inl == '1':
+				print('1')
+				if count < len(lines):
+					count += 1
+				#print(lines[count].strip())
+				vb_motor_lst((ph_to_morse(lines[count].strip())))
+				#tts = gtts.gTTS(lines[count].strip(), lang='en')
+				#audio_play(tts_save(lines[count].strip(), tts))
+			#zero_high = GPIO.input(zero)
+			#if zero_high == True:
+			if inl == '0':
+				print('0')
+				if count > 0:
+					count -= 1
+				#print(lines[count].strip())
+				vb_motor_lst((ph_to_morse(lines[count].strip())))
+				#tts = gtts.gTTS(lines[count].strip(), lang='en')
+				#audio_play(tts_save(lines[count].strip(), tts))
+
+'''
+def tts_save(dec_msg_str, tts):
+	rand_id = '{}{}.mp3'.format(dec_msg_str, random.randrange(100000, 1999999))
+	tts.save(rand_id)
+	return(rand_id)
+
+def audio_play(rand_id):
+	mixer.music.load('{}.mp3'.format(rand_id))
+	mixer.music.play()
+	audio = MP3('{}.mp3'.format(rand_id))
+	read_wait = audio.info.length
+	print(read_wait)
+	time.sleep(read_wait)
+'''
+
+def ph_to_morse(phrase):
+	phrase = phrase.upper()
+	#print(phrase)
+	# create dictionary
+	character = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9',' ', '+','á','é','í','ó','ú','ü','ñ']
+	code = ['01','1000','1010','100','0','0010','110','0000','00','0111','101','0100','11','10','111','0110','1101','010','000','1','001','0001','011','1001','1011','1100','11111','01111','00111','00011','00001','00000','10000','11000','11100','11110','*','+','01','0','00','111','001','001','10']
+
+	# reverse the previous dict as it's easier to access the keys
+	zipped_code_char = zip(character, code)
+	rev_morse_dict = dict(list(zipped_code_char))
+	# initiating a while loop
+	while True:
+		# empty list to store decoded message
+		enc_msg = []
+		enc_dic = {}
+
+		new_msg = []#list(phrase)
+
+		for char in phrase:
+			if char != ' ' and char != "'":
+				new_msg.append(char)
+				#if char != phrase[-1]:
+				new_msg.append('+')
+			elif char == ' ':
+				new_msg.append(' ')
+
+		#print(new_msg)
+		#print(''.join(new_msg))
+		# printing out the original message
+		print("Original message: " + phrase)
+
+		for j in range(0, len(new_msg)):
+			# get the alphanumeric alphabet based on the dict keys and append to dec_msg
+			if new_msg[j] in rev_morse_dict.keys():
+				enc_msg.append(rev_morse_dict[new_msg[j]])
+				#enc_dic[new_msg[j]] = rev_morse_dict[new_msg[j]]
+		#print(enc_dic)
+
+		# printing out the decoded message
+		enc_msg = ''.join(enc_msg)
+		print("Decoded Message is: " + enc_msg)
 		break
+	return(enc_msg)
 
-	if i > 2:
-		i = 0
+def vb_motor_lst(enc_msg):
+	for char in enc_msg:
+		if char == '1':
+			print('1')
+			winsound.Beep(2500, 1000)
+			#GPIO.output(vb, GPIO.HIGH)
+			#time.sleep(1)
+			#GPIO.output(vb, GPIO.LOW)
+		elif char == '0':
+			print('0')
+			winsound.Beep(2500, 500)
+			#GPIO.output(vb, GPIO.HIGH)
+			#time.sleep(.5)
+			#GPIO.output(vb, GPIO.LOW)
+		elif char == '+':
+			print('+')
+			#GPIO.output(vb, GPIO.LOW)
+			time.sleep(1)
+		elif char == '*':
+			print('*')
+			#GPIO.output(vb, GPIO.LOW)
+			time.sleep(2)
 
-	if i < 0:
-		i = 2
+'''
+def vb_motor_dic(enc_dic):
+	for key in enc_dic:
+		for j in enc_dic[key]:
+			if j == '0':
+				winsound.Beep(2500, 500)
+			elif j == '1':
+				winsound.Beep(2500, 1000)
+			elif j == '+':
+				time.sleep(.5)
+			elif j == '*':
+				time.sleep(.5)
+'''
 
-	if i == 0:
-		oled.text('- DICTIONARY', 2)
-		oled.text('  SPANISH', 3)
-		oled.text('  READ TXT', 4)
-		oled.show()
-
-	if i == 1:
-		oled.text('  DICTIONARY', 2)
-		oled.text('- SPANISH', 3)
-		oled.text('  READ TXT', 4)
-		oled.show()
-
-	if i == 2:
-		oled.text('  DICTIONARY', 2)
-		oled.text('  SPANISH', 3)
-		oled.text('- READ TXT', 4)
-		oled.show()
-
-oled.clear()
-print('i = {}'.format(i))
-
+i = 3
 if i == 0:
 	while True:
-		# create dictionary
-		character = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
-		code = ['01','1000','1010','100','0','0010','110','0000','00','0111','101','0100','11','10','111','0110','1101','010','000','1','001','0001','011','1001','1011','1100','11111','01111','00111','00011','00001','00000','10000','11000','11100','11110']
-
-		print('Enter Word:')
-		oled.text('DICTIONARY v1', 1)
-		oled.text('', 2)
-		oled.show()
-
-		phrase_lst = []
-		phrase_str = ''
-
-		while True:
-			oled.auto_show = True
-			oled.text(phrase_str.join(phrase_lst), 3)
-			one_high = GPIO.input(one)
-			if one_high == True:
-				print('1')
-				phrase_lst.append('1')
-				time.sleep(.4)
-
-			zero_high = GPIO.input(zero)
-			if zero_high == True:
-				print('0')
-				phrase_lst.append('0')
-				time.sleep(.4)
-
-			space_high = GPIO.input(space)
-			if space_high == True:
-				print('*')
-				phrase_lst.append('*')
-				time.sleep(.4)
-
-			enter_high = GPIO.input(enter)
-			if enter_high == True:
-				print('Enter')
-				oled.auto_show = False
-				break
-
-		phrase = ''.join(phrase_lst)
-		#print(phrase)
-		#print(phrase_lst)
-
-		# reverse the previous dict as it's easier to access the keys
-		zipped_code_char = zip(code,character)
-		rev_morse_dict = dict(list(zipped_code_char))
-		# initiating a while loop
-		while True:
-			# empty list to store original message
-			ori_msg = []
-			# empty list to store decoded message
-			dec_msg = []
-			# append input_msg (string) to ori_msg (string)
-			ori_msg.append(phrase)
-			# split each morse code by '*'
-			new_msg = phrase.split("*")
-
-			# printing out the original message
-			for line in ori_msg: # to print original message without the []
-				print("Original message: " + line + "\n")
-
-			# loop through each morse code representation
-			for j in range (0,len(new_msg)):
-				# get the alphanumeric alphabet based on the dict keys and append to dec_msg
-				if new_msg[j] in rev_morse_dict.keys():
-					dec_msg.append(rev_morse_dict[new_msg[j]])
-
-			# printing out the decoded message
-			dec_msg_str = ''.join(dec_msg)
-			print ("Decoded Message is: " + ''.join(dec_msg) + "\n")    # end the infinite while loop
-			break
-
-		oled.text('', 1)
-		oled.text('', 2)
-		oled.text('', 3)
-		oled.text('', 4)
-		oled.show()
-
-		try:
-			print('https://api.dictionaryapi.dev/api/v2/entries/en_US/{}'.format(dec_msg_str))
-			page = requests.get('https://api.dictionaryapi.dev/api/v2/entries/en_US/{}'.format(dec_msg_str))
-
-		except:
-			oled.clear()
-			print('Check Internet Connection!')
-			mixer.music.load('404.mp3') # 400 bad request
-			mixer.music.play()
-			oled.text('404', 1)
-			oled.text('Bad Internet Connection!', 2)
-			oled.show()
-			time.sleep(3)
-			oled.clear()
-			GPIO.cleanup()
-			exit()
-
-		stop = False
-		content = page.text
-		try:
-			obj = json.loads(content)
-			#print(obj)
-
-		except:
-			oled.clear()
-			print('Enter Valid Word!')
-			mixer.music.load('400.mp3') # 400 bad request
-			mixer.music.play()
-			oled.text('400', 1)
-			oled.text('Enter Valid Word!', 2)
-			oled.show()
-			time.sleep(3)
-			oled.clear()
-			stop = True
-
-		if stop == False:
-			xml = dicttoxml.dicttoxml(obj)
-			#print(xml)
-
-			i = 0
-			soup = BeautifulSoup(xml, features="xml")
-			#print(soup)
-			pos = soup.find_all('partOfSpeech', limit=4)
-
-			try:
-				pos_str = pos[i].get_text(strip=True)
-
-			except:
-				oled.clear()
-				print('Word Does Not Exist!')
-				mixer.music.load('400.mp3') # 400 bad request
-				mixer.music.play()
-				oled.text(dec_msg_str, 1)
-				oled.text('Does Not Exist!', 2)
-				oled.show()
-				time.sleep(3)
-				oled.clear()
-				stop = True
-
-			if stop == False:
-				definition =  soup.find_all('definition', limit=4)
-				def_str = definition[i].get_text(strip=True)
-
-				#print(pos_str)
-				#print(def_str)
-				dec_msg_pos_str = dec_msg_str + ' - ' + pos_str
-				#print(dec_msg_pos_str)
-				print(dec_msg_str + ' - ' + pos_str + ': ' + def_str)
-
-				out = re.sub("(.{16})", "\\1\n", def_str, 0, re.DOTALL)
-				if len(dec_msg_pos_str) > 20:
-					oled.text(dec_msg_str, 1)
-					oled.text(pos_str, 2)
-					oled.text(out, 3)
-					oled.show()
-				else:
-					oled.text(dec_msg_str + ' - ' + pos_str, 1)
-					oled.text(out, 2)
-					oled.show()
-
-				tts = gtts.gTTS(dec_msg_str + ' - ' + pos_str + ': ' + def_str, lang='en')
-				try:
-					tts.save('{}.mp3'.format(dec_msg_str))
-
-				except:
-					print('File Already Exists!')
-
-				mixer.music.load('{}.mp3'.format(dec_msg_str))
-				mixer.music.play()
-
-				audio = MP3('{}.mp3'.format(dec_msg_str))
-				read_wait = audio.info.length
-				print(read_wait)
-				time.sleep(read_wait)
-				oled.text('', 2)
-				oled.text('', 3)
-				oled.text('', 4)
-				oled.show()
-				oled.clear()
-
-if i == 1:
+		dictionary(input_gpio())
+elif i == 1:
 	while True:
-		# create dictionary
-		character = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
-		code = ['01','1000','1010','100','0','0010','110','0000','00','0111','101','0100','11','10','111','0110','1101','010','000','1','001','0001','011','1001','1011','1100','11111','01111','00111','00011','00001','00000','10000','11000','11100','11110']
+		spanish(input_gpio())
 
-		print('Enter Word:')
-		oled.text('SPANISH v1', 1)
-		oled.text('', 2)
-		oled.show()
+elif i == 2:
+	while True:
+		text_doc()
 
-		phrase_lst = []
-		phrase_str = ''
-
-		while True:
-			oled.auto_show = True
-			oled.text(phrase_str.join(phrase_lst), 3)
-			one_high = GPIO.input(one)
-			if one_high == True:
-				print('1')
-				phrase_lst.append('1')
-				time.sleep(.4)
-
-			zero_high = GPIO.input(zero)
-			if zero_high == True:
-				print('0')
-				phrase_lst.append('0')
-				time.sleep(.4)
-
-			space_high = GPIO.input(space)
-			if space_high == True:
-				print('*')
-				phrase_lst.append('*')
-				time.sleep(.4)
-
-			enter_high = GPIO.input(enter)
-			if enter_high == True:
-				print('Enter')
-				oled.auto_show = False
-				break
-
-		phrase = ''.join(phrase_lst)
-		#print(phrase)
-		#print(phrase_lst)
-
-		# reverse the previous dict as it's easier to access the keys
-		zipped_code_char = zip(code,character)
-		rev_morse_dict = dict(list(zipped_code_char))
-		# initiating a while loop
-		while True:
-			# empty list to store original message
-			ori_msg = []
-			# empty list to store decoded message
-			dec_msg = []
-			# append input_msg (string) to ori_msg (string)
-			ori_msg.append(phrase)
-			# split each morse code by '*'
-			new_msg = phrase.split("*")
-
-			# printing out the original message
-			for line in ori_msg: # to print original message without the []
-				print("Original message: " + line + "\n")
-
-			# loop through each morse code representation
-			for j in range (0,len(new_msg)):
-				# get the alphanumeric alphabet based on the dict keys and append to dec_msg
-				if new_msg[j] in rev_morse_dict.keys():
-					dec_msg.append(rev_morse_dict[new_msg[j]])
-
-			# printing out the decoded message
-			dec_msg_str = ''.join(dec_msg)
-			print ("Decoded Message is: " + ''.join(dec_msg) + "\n")    # end the infinite while loop
-			break
-
-		oled.text('', 1)
-		oled.text('', 2)
-		oled.text('', 3)
-		oled.text('', 4)
-		oled.show()
-
-		content = 'https://www.spanishdict.com/thesaurus/{}'.format(dec_msg_str)
-		hdr = {'User-Agent': 'Mozilla/5.0'}
-		req = Request(content, headers=hdr)
-		read_content = urlopen(req)
-		soup = BeautifulSoup(read_content, 'html.parser')
-		translation = soup.find('div', {'class' : '_3NkyA05U'}).get_text(strip=True).split('-')
-		definition = translation[0]
-		word = translation[1]
-
-		#print(dec_msg_str + ' = ' + word)
-		#print(word + ' - ' + definition)
-		dec_msg_trans_wrd = dec_msg_str + ' - ' + word
-		print('{} is {}: {}'.format(dec_msg_str, word, definition))
-
-		out = re.sub("(.{16})", "\\1\n", definition, 0, re.DOTALL)
-		if len(dec_msg_trans_wrd) > 20:
-			oled.text(dec_msg_str, 1)
-			oled.text(word, 2)
-			oled.text(definition, 3)
-			oled.show()
-		else:
-			oled.text(dec_msg_str + ' - ' + word, 1)
-			oled.text(definition, 2)
-			oled.show()
-
-		tts = gtts.gTTS('{} is {}: {}'.format(dec_msg_str, word, definition), lang='en')
-		try:
-			tts.save('{}.mp3'.format(dec_msg_str))
-
-		except:
-			print('File Already Exists!')
-
-		mixer.music.load('{}.mp3'.format(dec_msg_str))
-		mixer.music.play()
-
-		audio = MP3('{}.mp3'.format(dec_msg_str))
-		read_wait = audio.info.length
-		print(read_wait)
-		time.sleep(read_wait)
-		oled.text('', 2)
-		oled.text('', 3)
-		oled.text('', 4)
-		oled.show()
-		oled.clear()
-if i == 2:
-	print('READ TXT')
+elif i == 3:
+	while True:
+		math_solver()
